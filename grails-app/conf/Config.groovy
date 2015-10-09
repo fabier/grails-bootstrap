7f738c855c51640aad33b93976f9f98a801d494e
Pierre FABIER <pierre.fabier@gmail.com>
2015-10-09 15:57:18 +0200

    Ajout des éléments de SpringSecurity (Role, User)
    Modification de la configuration pour intégrer ces nouveaux éléments

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -117,10 +117,10 @@ log4j.main = {
 
 grails {
     mail {
-        host = "smtp.xxxxxx.com"
+        host = "smtp.XXXXXXXXXXX.com"
         port = 587
-        username = "xxxx@xxxxx.com"
-        password = "XXXXXXXXXXXXXX"
+        username = "XXXXXXXXXXX@XXXXXXXXXXX.com"
+        password = "XXXXXXXXXXX"
         props = ["mail.smtp.auth"                  : "true",
                  "mail.smtp.socketFactory.port"    : "587",
                  "mail.smtp.socketFactory.class"   : "javax.net.ssl.SSLSocketFactory",
@@ -145,7 +145,7 @@ grails.cache.config = {
     }
 
     cache {
-        name 'xxxxxxxxxxxxxx'
+        name 'XXXXXXXXXXX'
         eternal false
         overflowToDisk true
         maxElementsInMemory 10000
@@ -153,7 +153,7 @@ grails.cache.config = {
     }
 
     domain {
-        name 'xxxxxxxxxxxx.Role'
+        name 'XXXXXXXXXXX.Role'
         eternal false
         overflowToDisk false
         path "${System.getProperty("user.home")}/.ehcache"
@@ -167,5 +167,97 @@ grails.cache.config = {
 beans {
     cacheManager {
         shared = true
+    }
+}
+
+// Added by the Spring Security Core plugin:
+grails.plugin.springsecurity.userLookup.userDomainClassName = 'bootstrap.User'
+grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'bootstrap.UserRole'
+grails.plugin.springsecurity.authority.className = 'bootstrap.Role'
+grails.plugin.springsecurity.securityConfigType = "Annotation"
+grails.plugin.springsecurity.controllerAnnotations.staticRules = [
+        '/'              : ['permitAll'],
+        '/index'         : ['permitAll'],
+        '/index.gsp'     : ['permitAll'],
+        '/assets/**'     : ['permitAll'],
+        '/**/js/**'      : ['permitAll'],
+        '/**/css/**'     : ['permitAll'],
+        '/**/images/**'  : ['permitAll'],
+        '/**/favicon.ico': ['permitAll'],
+        '/monitoring'    : ['permitAll'],
+        '/monitoring/**' : ['permitAll']
+]
+
+grails.plugin.springsecurity.ui.register.postRegisterUrl = '/'
+grails.plugin.springsecurity.ui.register.emailFrom = 'XXXXXXXXXXX@XXXXXXXXXXX.com'
+grails.plugin.springsecurity.ui.register.emailSubject = 'XXXXXXXXXXX - Valider votre email'
+grails.plugin.springsecurity.ui.register.defaultRoleNames = ['ROLE_USER']
+grails.plugin.springsecurity.ui.password.validationRegex = '^.*(?=.*[a-zA-Z\\d]).*$' // Au moins quelques caractères
+grails.plugin.springsecurity.ui.password.minLength = 4
+grails.plugin.springsecurity.ui.password.maxLength = 64
+grails.plugin.springsecurity.userLookup.usernamePropertyName = 'email'
+grails.plugin.springsecurity.logout.postOnly = false
+
+
+grails {
+    plugin {
+        springsecurity {
+
+            ui {
+
+                encodePassword = false
+
+                register {
+                    emailBody = '''\
+Bonjour $user.username,<br/>
+<br/>
+Vous venez de créer un compte sur <a href="http://www.XXXXXXXXXXX.com">XXXXXXXXXXX</a> et nous vous en remercions !<br/>
+<br/>
+Merci de <strong><a href="$url">cliquer ici</a></strong> pour terminer la procédure d'enregistrement, ou copier coller l'adresse suivante dans votre navigateur :<br/>
+$url<br/>
+<br/>
+Merci de ne pas répondre à ce message automatique.<br/>
+<br/>
+L'équipe XXXXXXXXXXX
+'''
+                    emailFrom = 'XXXXXXXXXXX <XXXXXXXXXXX@XXXXXXXXXXX.com>'
+                    emailSubject = 'XXXXXXXXXXX - Création de compte'
+                    defaultRoleNames = ['ROLE_USER']
+                    postRegisterUrl = null // use defaultTargetUrl if not set
+                    emailTo = 'contact@XXXXXXXXXXX.com'
+                    emailBodyToInternalEmailAccount = '''\
+Bonjour,<br/>
+<br/>
+Un nouvel utilisateur vient d'être créé sur <a href="http://www.XXXXXXXXXXX.com">XXXXXXXXXXX</a>.<br/>
+<br/>
+  Nom : <b>$user.username</b><br/>
+Email : <b>$user.email</b><br/>
+<br/>
+Merci de ne pas répondre à ce message automatique.<br/>
+<br/>
+L'équipe XXXXXXXXXXX
+'''
+                }
+
+                forgotPassword {
+                    emailBody = '''\
+Hi $user.username,<br/>
+<br/>
+Vous, ou quelqu'un se faisant passer pour vous, venez de faire une demande de mise à zéro de votre mot de passe sur <a href="http://www.XXXXXXXXXXX.com">XXXXXXXXXXX</a>.<br/>
+<br/>
+Si vous n'avez pas fait cette demande, alors ignorez ce message et supprimez le, aucun changement de sera appliqué à votre compte.<br/>
+<br/>
+Si vous êtes bien celui qui a fait la demande, alors <a href="$url">cliquez ici</a> pour remettre à zéro votre mot de passe.
+<br/>
+Merci de ne pas répondre à ce message automatique.<br/>
+<br/>
+L'équipe XXXXXXXXXXX
+'''
+                    emailFrom = 'XXXXXXXXXXX <XXXXXXXXXXX@XXXXXXXXXXX.com>'
+                    emailSubject = 'XXXXXXXXXXX - Réinitialisation du mot de passe'
+                    postResetUrl = null // use defaultTargetUrl if not set
+                }
+            }
+        }
     }
 }
